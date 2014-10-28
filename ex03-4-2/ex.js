fea9eff233eadae7ea9c402b0c3157eac9fa35a6
yagihashoo <yagihashoooooo@gmail.com>
2014-10-28 11:36:28 +0900

    リセットボタン追加

--- ex03-4-2/ex.js
+++ ex03-4-2/ex.js
@@ -1,6 +1,10 @@
 window.onload = function() {
-  var button = document.getElementsByTagName("button")[0];
-  button.onclick = function() {
+  var button_ok = document.getElementsByTagName("button")[0];
+  var button_reset = document.getElementsByTagName("button")[1];
+  button_ok.onclick = function() {
     document.getElementsByTagName("h1")[0].innerText = "JavaScriptの説明";
+  };
+  button_reset.onclick = function() {
+    document.getElementsByTagName("h1")[0].innerText = "JavaScriptとは";
   };
 };
